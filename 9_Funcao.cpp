9b16ce1efe1f3db3672cbfd01eef8a0119b2adc4
MaratonaProgramacaoFEI <128613712+MaratonaProgramacaoFEI@users.noreply.github.com>
2023-04-12 09:01:15 -0300

    Update 9_Funcao.cpp

--- 9_Funcao.cpp
+++ 9_Funcao.cpp
@@ -29,3 +29,35 @@ int main(){
     
     soma(); //chama a funcao, o codigo presente nela eh executado;
 }
+
+//METODO2:
+
+#include <iostream>
+using namespace std;
+
+int a,b; //ao definir variaveis fora da main, tornam-se globais, de modo que todos podem acessa-la;
+
+void soma(); //primeiro define-se a funcao;
+
+int main(){
+    /*
+    Uma função é um bloco de código responsável por realizar algo bem específico dentro
+    de um programa, como fazer uma conta por exemplo;
+    Ha duas maneiras de fazer uma funcao;
+    */
+
+    //recebendo os numeros:
+    cout << "Digite o primeiro numero: ";
+    cin >> a;
+    cout << endl;
+    cout << "Digite o segundo numero: ";
+    cin >> b;
+    cout << endl;
+
+    soma(); //chama a funcao, o codigo presente nela eh executado;
+}
+
+void soma(){
+    //a funcao, ao ser chamada, retorna a soma de dois valores recebidos na main;
+    cout << "Soma entre os dois numeros: " << a + b << endl;
+}
