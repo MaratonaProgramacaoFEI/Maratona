c831d537cfc1269b8d8161e3e36738ff379b0972
MaratonaProgramacaoFEI <128613712+MaratonaProgramacaoFEI@users.noreply.github.com>
2023-04-12 09:28:58 -0300

    Update 6_Matriz.cpp

--- 6_Matriz.cpp
+++ 6_Matriz.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    /*
+    Matriz sao arrays de arrays;
+    */
+
+    //definindo a matriz 3x3:
+    int matriz[3][3];
+    //adicionando valores a matriz:
+    for(int linha = 0;linha<3;linha++){
+        for(int coluna = 0;coluna<3;coluna++){
+            cin >> matriz[linha][coluna];
+        }
+    }
+    //printando a matriz:
+    for(int linha = 0;linha<3;linha++){
+        for(int coluna = 0;coluna<3
+        ;coluna++){
+            cout << matriz[linha][coluna] << " ";
+        }
+        cout << endl;
+    }
+}
